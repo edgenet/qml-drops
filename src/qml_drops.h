78c5cbdb224e09856b067d3218a9ee73836a97ca
Joe McIlvain <joe.eli.mac@gmail.com>
2014-10-30 16:27:56 -0700

    Don't use QThread for Drops - it already runs in the background

--- src/qml_drops.h
+++ src/qml_drops.h
@@ -7,24 +7,39 @@
 #include <drops.h>
 
 
-class Drops : public QThread
+class Drops : public QObject
 {
   Q_OBJECT
-  void _() {};
   
   Q_PROPERTY(QString path READ path WRITE setPath)
   
   QString m_path;
   
-  drops_t*  service = NULL;
+public slots:
   
-  zctx_t*  kill_ctx = NULL;
-  void*   kill_sock = NULL;
-  void* killer_sock = NULL;
+  QString path() { return m_path; };
+  void setPath(const QString &path) {
+    m_path = path;
+    if(running)
+      start();
+  };
   
-  void new_service() {
-    if(service) drops_destroy(&service);
-    service = drops_new(m_path.toLatin1().data());
+private:
+  
+  drops_t* self;
+  bool running;
+  
+public slots:
+  
+  void start() {
+    drops_destroy(&self);
+    self = drops_new(m_path.toLatin1().data());
+    running = true;
+  };
+  
+  void stop() {
+    drops_destroy(&self);
+    running = false;
   }
   
 public:
@@ -32,70 +47,13 @@ public:
   Drops() {
     m_path = QString("/tmp/drops"); // Default path, to be overriden by user
     
-    kill_ctx = zctx_new();
-    
-    killer_sock = zsocket_new(kill_ctx, ZMQ_PUSH);
-    kill_sock   = zsocket_new(kill_ctx, ZMQ_PULL);
-    
-    zsocket_bind(killer_sock,  "inproc://kill");
-    zsocket_connect(kill_sock, "inproc://kill");
-  }
-  
-  ~Drops() {
-    zsocket_destroy(kill_ctx, killer_sock);
-    zsocket_destroy(kill_ctx, kill_sock);
-    
-    zctx_destroy(&kill_ctx);
-  }
-  
-public slots:
-  
-  QString path() { return m_path; }
-  void setPath(const QString& p) { m_path = p; new_service(); }
-  
-  void start() {
-    if(!service) new_service();
-    QThread::start();
+    self = NULL;
+    start();
   };
   
-  void stop() {
-    zstr_send(killer_sock, "KILL");
-    wait();
-  }
-  
-private:
-  
-  void run() Q_DECL_OVERRIDE {
-    qDebug() << "Running...";
-    
-    void* drops_sock = drops_socket(service);
-    
-    zpoller_t* poller = zpoller_new(drops_sock, kill_sock, NULL);
-    
-    while (true) {
-      void* which = zpoller_wait(poller, -1);
-      
-      if(which == drops_socket(service)) {
-        zmsg_t* msg = drops_recv(service);
-        zmsg_dump(msg);
-        zmsg_destroy(&msg);
-      }
-      else if(which == kill_sock) {
-        zstr_recv(kill_sock);
-        break;
-      }
-      else {
-        qWarning() << "Error while polling in thread" \
-                   << QThread::currentThread();
-        break;
-      }
-    }
-    
-    qDebug() << "Done.";
-    
-    exit(0);
-  }
-  
+  ~Drops() {
+    stop();
+  };
 };
 
 #endif

c6f136b0b7e2d648810cf8f2217df0587bddfbf8
Joe McIlvain <joe.eli.mac@gmail.com>
2014-10-30 20:25:13 -0700

    Use 4-space indent for C++ code
    --HG--
    extra : amend_source : 9a8cd33cfc71a599d7536724e09869ba70462051

--- src/dropsplugin.h
+++ src/dropsplugin.h
@@ -8,13 +8,13 @@
 
 class ZMQPlugin : public QQmlExtensionPlugin
 {
-  Q_OBJECT
-  Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
-  
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
+    
 public:
-  void registerTypes(const char *uri)
-  {
-    qmlRegisterType<Drops> (uri, 1, 0, "Drops");
-    qmlRegisterType<DropsAttached>();
-  };
+    void registerTypes(const char *uri)
+    {
+        qmlRegisterType<Drops> (uri, 1, 0, "Drops");
+        qmlRegisterType<DropsAttached>();
+    };
 };
--- src/qml_drops.h
+++ src/qml_drops.h
@@ -11,76 +11,76 @@ class Drops;
 
 class DropsAttached : public QObject
 {
-  Q_OBJECT
-  
-  QObject* m_attached;
-  
+    Q_OBJECT
+    
+    QObject* m_attached;
+    
 public:
-  
-  DropsAttached(QObject* attached) { m_attached = attached; };
-  
+    
+    DropsAttached(QObject* attached) { m_attached = attached; };
+    
 public slots:
-  
-  void selfTest(bool verbose) {
-    return drops_test(verbose);
-  };
+    
+    void selfTest(bool verbose) {
+        return drops_test(verbose);
+    };
 };
 
 
 class Drops : public QObject
 {
-  Q_OBJECT
-  
-  Q_PROPERTY(QString path READ path WRITE setPath)
-  Q_PROPERTY(bool running READ running)
-  
-  QString m_path;
-  bool m_running;
-  
-public slots:
-  
-  QString path() { return m_path; };
-  void setPath(const QString &path) {
-    m_path = path;
-    if(m_running)
-      start();
-  };
-  
-  bool running() { return m_running; };
-  
-private:
-  
-  drops_t* self;
-  
-public slots:
-  
-  void start() {
-    drops_destroy(&self);
-    self = drops_new(m_path.toLatin1().data());
-    m_running = !!self;
-  };
-  
-  void stop() {
-    drops_destroy(&self);
-    m_running = false;
-  }
-  
-public:
-  
-  Drops() {
-    m_path = QString("/tmp/drops"); // Default path, to be overriden by user
-    m_running = false;
+    Q_OBJECT
     
-    self = NULL;
-  };
-  
-  ~Drops() {
-    stop();
-  };
-  
-  static QObject* qmlAttachedProperties(QObject* object) {
-    return new DropsAttached(object);
-  }
+    Q_PROPERTY(QString path READ path WRITE setPath)
+    Q_PROPERTY(bool running READ running)
+    
+    QString m_path;
+    bool m_running;
+    
+public slots:
+    
+    QString path() { return m_path; };
+    void setPath(const QString &path) {
+        m_path = path;
+        if(m_running)
+            start();
+    };
+    
+    bool running() { return m_running; };
+    
+private:
+    
+    drops_t* self;
+    
+public slots:
+    
+    void start() {
+        drops_destroy(&self);
+        self = drops_new(m_path.toLatin1().data());
+        m_running = !!self;
+    };
+    
+    void stop() {
+        drops_destroy(&self);
+        m_running = false;
+    }
+    
+public:
+    
+    Drops() {
+        m_path = QString("/tmp/drops"); // Default path, to be overriden by user
+        m_running = false;
+        
+        self = NULL;
+    };
+    
+    ~Drops() {
+        stop();
+    };
+    
+    static QObject* qmlAttachedProperties(QObject* object) {
+        return new DropsAttached(object);
+    }
 };
 
 
